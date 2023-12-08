334d5c7c6942d7259e2b74265df65a65f9925046
ATLCode <aleksanteri.leinonen@gmail.com>
2023-12-08 08:31:08 +0200

    Added vueuse, sass and fixed end of line error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/eslint-module", "@pinia/nuxt", "@nuxtjs/supabase"],
+  modules: [
+    "@nuxtjs/eslint-module",
+    "@pinia/nuxt",
+    "@nuxtjs/supabase",
+    "@vueuse/nuxt",
+  ],
   supabase: {
     // Options
     redirect: false,
