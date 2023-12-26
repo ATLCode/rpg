57e18fcc4266d7bf1fd76283b435e9ce61a84330
ATLCode <aleksanteri.leinonen@gmail.com>
2023-12-26 02:06:16 +0200

    Basics for Button and Field

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -1,5 +1,31 @@
 <template>
-  <button type="button"></button>
+  <button type="button">{{ props.text }}</button>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// Pure type annotations or not?
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: "",
+  },
+  color: {
+    type: String,
+    default: "",
+  },
+  variant: {
+    type: String,
+    default: "",
+  },
+  // Readonly separately?
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  rules: {
+    type: Array,
+    default: null,
+  },
+});
+</script>
 <style lang="scss" scoped></style>
--- components/AppField.vue
+++ components/AppField.vue
@@ -1,5 +1,33 @@
 <template>
   <input type="text" />
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  value: {
+    type: String,
+    default: "",
+  },
+  color: {
+    type: String,
+    default: "",
+  },
+  variant: {
+    type: String,
+    default: "",
+  },
+  // Readonly separately?
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  rules: {
+    type: Array,
+    default: null,
+  },
+});
+</script>
 <style lang="scss" scoped></style>
