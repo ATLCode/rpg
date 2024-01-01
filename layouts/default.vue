eb2755a2140826b87443f9e0dafa6b2a01c8daca
ATLCode <74410540+ATLCode@users.noreply.github.com>
2024-01-01 23:29:10 +0200

    Theming (#19)
    * Layounts, Partly fonts, Partly endOfLine
    
    * Fonts set up
    
    * Warnin fix
    
    * Basic colors variables
    
    * Layout background color, global css etc
    
    * Comment

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="default-container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.default-container {
+  background-color: $background;
+}
+</style>
--- pages/saves.vue
+++ pages/saves.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <div>saves page</div>
+    <div class="test">saves page</div>
   </div>
 </template>
 <script lang="ts" setup>
 definePageMeta({ middleware: ["auth"] });
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.test {
+  font-family: "Rubik Doodle Triangles";
+}
+</style>
