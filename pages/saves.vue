d06c42cf02674bc14244c093b40da6f75a6a39ec
ATLCode <aleksanteri.leinonen@gmail.com>
2024-01-20 07:08:22 +0200

    character creation and saves refactoring

--- pages/saves.vue
+++ pages/saves.vue
@@ -1,75 +1,55 @@
 <template>
-  <div>
-    <h1>Saves</h1>
-    <div v-for="saveSlot of saves" :key="saveSlot.id">
-      {{ saveSlot.save }}
-      <button @click="deleteSave(saveSlot.id)">Delete</button>
-      <button hidden @click="updateSave(saveSlot.id)">Update Save</button>
+  <div class="sContainer">
+    <div class="sSlots">
+      <div v-for="saveSlot of saveStore.saves" :key="saveSlot.id" class="sSlot">
+        <div>Save Slot</div>
+        <div v-if="saveSlot.save" class="saveContent">
+          {{ saveSlot.save }}
+          <AButton @click="saveStore.deleteSave(saveSlot.id)">Delete</AButton>
+          <AButton hidden @click="saveStore.updateSave(saveSlot.id)"
+            >Update Save</AButton
+          >
+        </div>
+        <div v-else-if="!saveSlot.save" class="saveEmpty">
+          <!-- GoTo /character-creation -->
+          <AButton @click="navigateTo('/character-creation')">New Save</AButton>
+        </div>
+      </div>
     </div>
-    <input v-model="newSave" type="text" />
-    <button @click="createSave">New Save</button>
-    <AButton>Log Out</AButton>
+    <div class="otherStuff">
+      <AButton>Log Out</AButton>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
+import { useSaveStore } from "@/stores/save";
+const saveStore = useSaveStore();
+saveStore.getUserSaves();
+
 definePageMeta({ middleware: ["auth"] });
-const saves = ref<any[]>([]);
-const newSave = ref("");
-
-async function createSave() {
-  try {
-    await $fetch("/api/saves/add", {
-      method: "POST",
-      body: {
-        save: newSave.value,
-      },
-    });
-    newSave.value = "";
-    getUserSaves();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function updateSave(saveId: number) {
-  try {
-    await $fetch("/api/saves/update", {
-      method: "POST",
-      body: {
-        saveId,
-        save: "saveeemee",
-      },
-    });
-    getUserSaves();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function getUserSaves() {
-  const { data } = await useFetch("/api/saves/get-for-user");
-  saves.value = data.value as any[];
-}
-
-async function deleteSave(saveId: number) {
-  try {
-    await $fetch("/api/saves/delete", {
-      method: "POST",
-      body: {
-        saveId,
-        save: "saveeemee",
-      },
-    });
-    getUserSaves();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-getUserSaves();
 </script>
 <style lang="scss" scoped>
-.test {
-  font-family: "Rubik Doodle Triangles";
+.sContainer {
+  min-height: 100vh;
+  min-width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: var(--text);
+  gap: 1rem;
+  .sSlots {
+    width: 30vw;
+    display: flex;
+    flex-direction: column;
+  }
+  .sSlot {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid red;
+    padding: 1rem;
+  }
 }
 </style>
