fe3d624fe6f59be2b8b23c77ac4a0363fbe18264
ATLCode <74410540+ATLCode@users.noreply.github.com>
2024-01-29 08:07:28 +0200

    World store, locations and paths game files (#32)

---
+++ game/paths.ts
@@ -0,0 +1,21 @@
+type Path = {
+  id: number;
+  name: string;
+  locations: [number, number];
+  encounters: number[]; // Give type later
+  reqSkills: null;
+  reqItem: null;
+  reqQuest: null;
+};
+
+export const paths: Path[] = [
+  {
+    id: 1,
+    name: "Forest Path",
+    locations: [1, 2],
+    encounters: [],
+    reqSkills: null,
+    reqItem: null,
+    reqQuest: null,
+  },
+];
