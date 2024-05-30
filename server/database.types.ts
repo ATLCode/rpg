b7353ce8d7ec4e0015147fab1e7c6850e4fcaea2
Verner Villikka <33653136+Vennufius@users.noreply.github.com>
2024-05-30 19:48:03 +0300

    Load latest save if user his logged in and has saves (#75)

--- server/database.types.ts
+++ server/database.types.ts
@@ -1,3 +1,11 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[];
+
 export type Database = {
   public: {
     Tables: {
@@ -6,18 +14,21 @@ export type Database = {
           created_at: string;
           id: number;
           saveData: string;
+          updated_at: string;
           user_id: string;
         };
         Insert: {
           created_at?: string;
           id?: number;
           saveData: string;
+          updated_at?: string;
           user_id: string;
         };
         Update: {
           created_at?: string;
           id?: number;
           saveData?: string;
+          updated_at?: string;
           user_id?: string;
         };
         Relationships: [
